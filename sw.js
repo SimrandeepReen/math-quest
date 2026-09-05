47c573dce39f923c464a734e575e868756a72c68
SimrandeepReen <simranccet@gmail.com>
2026-09-05 10:08:20 +0200

    Cache Playground Adventure assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='math-quest-v3';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='math-quest-v3.1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
