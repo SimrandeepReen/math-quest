e3862e7c8d2a082af361dacd07215c31d927068b
SimrandeepReen <simranccet@gmail.com>
2026-09-05 10:13:07 +0200

    Release V3.2 with cache-busted assets

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE='math-quest-v3.1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='math-quest-v3.2';
+const ASSETS=['./','./index.html','./styles.css?v=3.2','./app.js?v=3.2','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
   if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));
     return;
   }
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
